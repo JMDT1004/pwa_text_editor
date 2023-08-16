9e8ba99c52fa20ade6697579fc469aea304e4ed6
JT <raikiri24@gmail.com>
2023-08-16 11:11:29 -0400

    update sw cache

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,17 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+const assetCache = new CacheFirst({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxEntries: 60,
+      maxAgeSeconds: 24 * 60 * 60, 
+    }),
+  ],
+});
+
+registerRoute(assetCache);
